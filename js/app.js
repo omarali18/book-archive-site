c9f64b173fed4104d3b8f621ca91d75cbfe33d59
omarali18 <omarali1814000@gmail.com>
2021-09-02 17:59:16 +0600

    some edit

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ const searchClick = () => {
     toggleSpinner("block")
     const inputField = document.getElementById("input-field").value;
     allClear()
+
+    // Using triple equal condition for error handling
     if (inputField.length === 0) {
         document.getElementById("error").innerHTML = `
         <p class="bg-white p-2 text-center rounded text-danger fw-bold fs-2">Please write a book name</p>
@@ -39,25 +41,31 @@ const createDynamicUrl = value => {
 
 // Displays data loaded from the API
 const displayBooks = (books, searchResult) => {
+
+    // Using triple equal condition for error handling
     if (books.length === 0) {
         document.getElementById("error").innerHTML = `
         <p class="bg-white p-2 text-center rounded text-danger fw-bold fs-2">Result not found...</p>
         `;
         toggleSpinner("none")
+        return;
     }
     const resultContainer = document.getElementById("display-result");
     document.getElementById("search-count").innerHTML = `
     <p>${searchResult}/${books.length}</p>
     `;
+
+    // Using forEach loop for bonus 
     books.forEach(book => {
+        console.log(book);
         const div = document.createElement("div");
         div.classList.add("col");
         div.innerHTML = `
         <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <img height="300" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Book Name : <span class="value-color">${book.title}</span></h5>
-                <h5 class="card-title">Author : <span class="value-color">${book.author_name ? book.author_name : ""}</span></h5>
+                <h5 class="card-title">Author : <span class="value-color">${book.author_name ? book.author_name[0] : ""}</span></h5>
                 <h5 class="card-title">Publisher : <span class="value-color">${book.publisher ? book.publisher[0] : ""}</span></h5>
                 <h5 class="card-title">First publish : <span class="value-color">${book.first_publish_year ? book.publish_year[0] : ""}</span> </h5>
             </div>
