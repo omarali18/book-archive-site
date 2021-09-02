40dce34a9e1d33980d82f7173607f028e2efc223
omarali18 <omarali1814000@gmail.com>
2021-09-02 11:53:52 +0600

    error messege added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
+
+// Function for spinner
+const showSpinner = direction => {
+    document.getElementById(id = "spinner").style.display = direction;
+}
+
 // onclick function
 const searchClick = () => {
+    showSpinner("block")
     const inputField = document.getElementById("input-field").value;
     document.getElementById("input-field").value = "";
+    document.getElementById("error").textContent = "";
+    if (inputField.length === 0) {
+        document.getElementById("error").innerHTML = `
+        <p class="bg-danger p-2 text-center rounded text-white">Please write a book name</p>
+        `;
+        showSpinner("none")
+        return;
+    }
     createDynamicUrl(inputField);
 }
 // Convar url with async await
@@ -9,11 +24,25 @@ const fetchUrl = async urls => {
     const res = await fetch(urls);
     const data = await res.json();
     console.log(data);
-    console.log(data.docs);
+    displayBooks(data.docs);
 }
 //javascript
 // Create Dynamic url
 const createDynamicUrl = value => {
     url = `https://openlibrary.org/search.json?q=${value}`
     fetchUrl(url);
+}
+
+// Displays data loaded from the API
+const displayBooks = books => {
+    if (books.length === 0) {
+        document.getElementById("error").innerHTML = `
+        <p class="bg-danger p-2 text-center rounded text-white">Result not found...</p>
+        `;
+        showSpinner("none")
+    }
+    console.log(books);
+    console.log(books.length);
+    const resultContainer = document.getElementById("display-result");
+
 }
